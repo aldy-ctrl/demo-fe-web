0e3d2810964021dba43441680cf7b222f7b796bb
Achmad Nur Fauzi <61571729+FauziPay@users.noreply.github.com>
2022-11-23 17:23:22 +0700

    update tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
